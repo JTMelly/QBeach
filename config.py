2690075043e5c85b838695630f6d75cbab3ce20e
Jacob Melly <jacobtristan.melly@unive.it>
2026-04-08 11:05:56 +0200

    first save of simple working BathyBuilder and ModelMaker

---
+++ config.py
@@ -0,0 +1,17 @@
+# Default values at load/reset
+DEFAULT_SETTINGS = {
+    'distx': 400.0,
+    'disty': 900.0,
+    'resx': 25.0,
+    'resy': 25.0,
+    'rotation': 0,
+    'skip_x': 1,
+    'skip_y': 1,
+    'duration': 600,
+    'tide': 0.0,
+    'Hm0': 1.0,
+    'Tp': 6.0,
+    'mainAngle': 0.0,
+    'spread': 10,
+    'gammajsp': 3.3
+}
